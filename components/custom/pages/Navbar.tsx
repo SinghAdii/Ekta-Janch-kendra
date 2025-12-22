096a64d3292583123966019bb181456ee806a502
Sujal Singhal <tinsusinghal@gmail.com>
2025-12-22 16:46:04 +0530

    add some colours according to the company figma design

---
+++ components/custom/pages/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="flex w-full justify-between mx-6">
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
+    </div>
+  );
+}
